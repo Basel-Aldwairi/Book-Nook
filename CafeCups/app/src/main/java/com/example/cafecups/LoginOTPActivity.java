0a03e4b6260c7d99f8a45ea9448bf5fb31fe0a3a
Basel-Aldwairi <aldwairi.basel0@gmail.com>
2025-05-01 12:16:43 +0300

    Upload to Github

---
+++ CafeCups/app/src/main/java/com/example/cafecups/LoginOTPActivity.java
@@ -0,0 +1,32 @@
+package com.example.cafecups;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginOTPActivity extends AppCompatActivity {
+
+    String phoneNumber;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login_otp);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //this is a comment
+
+        phoneNumber = getIntent().getExtras().getString("phone");
+        Toast.makeText(this, phoneNumber, Toast.LENGTH_SHORT).show();
+
+    }
+}
